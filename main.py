d5e9ae3774ed6edfcb1139716d2eccf5f0c9d21a
Betojr04 <betomasia12@gmail.com>
2024-12-03 11:41:28 -0700

    added routes for projects page and contact page extended the base html to the contact and projects page added basic bootstrap navbar to the base html file

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def server_check():
+def home_page():
     return render_template("index.html")
+
+
+@app.route("/projects")
+def projects_page():
+    return render_template("projects.html")
+
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
