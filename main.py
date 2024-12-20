bebd5a11ed270af466498418d3ec7870cef51f51
Betojr04 <betomasia12@gmail.com>
2024-12-19 18:03:07 -0700

    added content to all the newly modulated structure adjusted the routes in the flask app to account for the new folder structure

--- main.py
+++ main.py
@@ -5,32 +5,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    return render_template("HomePage/index.html")
 
 
 @app.route("/projects")
 def projects_page():
-    return render_template("projects.html")
+    return render_template("ProjectsPage/projects.html")
 
 
 @app.route("/contact")
 def contact_page():
-    return render_template("contact.html")
+    return render_template("ContactPage/contact.html")
 
 
 @app.route("/resume")
 def resume_page():
-    return render_template("resume.html")
+    return render_template("ResumePage/resume.html")
 
 
 @app.route("/freelance")
 def freelaner_page():
-    return render_template("freelancer.html")
+    return render_template("FreelancingPage/freelancer.html")
 
 
 @app.route("/recruiter")
 def recruiter_page():
-    return render_template("recruiter.html")
+    return render_template("RecruiterPage/recruiter.html")
 
 
 if __name__ == "__main__":
